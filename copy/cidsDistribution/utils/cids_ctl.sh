744ed533d846fc12e8bff52bd165ff8f26df72ed
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2018-09-12 18:04:43 +0200

    i

--- copy/cidsDistribution/utils/cids_ctl.sh
+++ copy/cidsDistribution/utils/cids_ctl.sh
@@ -1,80 +1,138 @@
 #!/bin/bash
 
-# General cids server control script
-
-#export CIDS_DISTRIBUTION_DIR=/cidsDistribution <- already set in docker image
-#export CIDS_SERVER_DIR=$CIDS_DISTRIBUTION_DIR/server <- already set in docker image
-
-export CIDS_LOCAL_DIR=${CIDS_LIB_DIR}/local${CIDS_ACCOUNT_EXTENSION}
 export CIDS_STARTER_DIR=${CIDS_LIB_DIR}/starter${CIDS_ACCOUNT_EXTENSION}
 
 ### FUNCTIONS 
 # -----------------------------------------------------------------------------------------
 
-pattern='^[0-9]{3}_.+$'
-cd $CIDS_SERVER_DIR
+SERVERS_PATH=${CIDS_DISTRIBUTION_DIR}/server
+
+list_server_dirs() {
+    cd ${SERVERS_PATH}
+    ls -1 . | grep -E "^[0-9]{3}_.*$" | sort
+}
+
+list_services() {
+    for SERVER_DIR in $(list_server_dirs); do
+        SERVICE=${SERVER_DIR:4}
+        ENV_FILE=${SERVERS_PATH}/${SERVER_DIR}/ENV
+        if [ -f ${ENV_FILE} ]; then
+            source ${ENV_FILE}
+        fi
+
+        echo ${SERVICE}
+    done
+}
+
+get_server_dir() {
+    _SERVICE=$1
+
+    for SERVER_DIR in $(list_server_dirs); do
+        SERVICE=${SERVER_DIR:4}
+        ENV_FILE=${SERVERS_PATH}/${SERVER_DIR}/ENV
+        if [ -f ${ENV_FILE} ]; then
+            source ${ENV_FILE}
+        fi
+
+        if [ "${_SERVICE}" == "${SERVICE}" ]; then
+            echo ${SERVER_DIR}
+            exit 0
+        fi        
+    done
+}
+
+start_server() {
+    SERVICE=$1
+    SERVER_DIR=$(get_server_dir ${SERVICE})
+    SERVICE_DIR=${SERVERS_PATH}/${SERVER_DIR}
+    STARTER_JAR=${SERVICE}-starter.jar
+    SLEEP_BEFORE_START=1
+    ENV_FILE=${SERVERS_PATH}/${SERVER_DIR}/ENV
+    if [ -f ${ENV_FILE} ]; then
+        source ${ENV_FILE}
+    fi
+
+    sleep ${SLEEP_BEFORE_START}
+
+    export SERVICE SERVICE_DIR STARTER_JAR START_OPTIONS XMS XMX
+    ${CIDS_DISTRIBUTION_DIR}/utils/_cids_service_ctl.master.sh start
+}
+
+stop_server() {
+    SERVICE=$1
+    SERVER_DIR=$(get_server_dir ${SERVICE})    
+    SERVICE_DIR=${SERVERS_PATH}/${SERVER_DIR}
+    SLEEP_BEFORE_STOP=1
+    ENV_FILE=${SERVERS_PATH}/${SERVER_DIR}/ENV
+    if [ -f ${ENV_FILE} ]; then
+        source ${ENV_FILE}
+    fi
+
+    sleep ${SLEEP_BEFORE_STOP}
+
+    export SERVICE SERVICE_DIR
+    ${CIDS_DISTRIBUTION_DIR}/utils/_cids_service_ctl.master.sh stop
+}
+
+log() {
+    SERVICE=$1
+    SERVER_DIR=$(get_server_dir ${SERVICE})    
+    SERVICE_DIR=${SERVERS_PATH}/${SERVER_DIR}
+    ENV_FILE=${SERVERS_PATH}/${SERVER_DIR}/ENV
+    if [ -f ${ENV_FILE} ]; then
+        source ${ENV_FILE}
+    fi
+
+    cat ${SERVICE_DIR}/${SERVICE}.out
+}
+
+follow() {
+    SERVICE=$1
+    SERVER_DIR=$(get_server_dir ${SERVICE})    
+    SERVICE_DIR=${SERVERS_PATH}/${SERVER_DIR}
+    ENV_FILE=${SERVERS_PATH}/${SERVER_DIR}/ENV
+    if [ -f ${ENV_FILE} ]; then
+        source ${ENV_FILE}
+    fi
+
+    tail -f ${SERVICE_DIR}/${SERVICE}.out
+}
+
+### OPTIONS
+# -----------------------------------------------------------------------------------------
 
 case "$1" in
     
-    stop)
-        for SERVICE_DIR in *; do
-            if [[ -d $SERVICE_DIR && $SERVICE_DIR =~ $pattern ]]; then
-                SERVICE=$(cat ${CIDS_SERVER_DIR}/$SERVICE_DIR/cs_ctl.sh | grep SERVICE | cut -f 2 -d "=")
-                if [[ -z $2 || $2 = $SERVICE ]]; then
-                    echo -e "\e[32mINFO\e[39m: stopping \e[1m$SERVICE\e[0m cids server process in $SERVICE_DIR/"
-                    ${CIDS_SERVER_DIR}/$SERVICE_DIR/cs_ctl.sh $1
-                    sleep 2
-                fi
-            fi
-        done
-
-        sleep 5
-        ps -ef|grep 'D'${CIDS_ACCOUNT_EXTENSION}
-
-        # not necessary anymore -> _cids_service_ctl.master.sh should take care to kill services
-        for SERVICE_DIR in *; do
-            if [[ -d $SERVICE_DIR && $SERVICE_DIR =~ $pattern ]]; then
-                SERVICE=$(cat ${CIDS_SERVER_DIR}/$SERVICE_DIR/cs_ctl.sh | grep SERVICE | cut -f 2 -d "=")
-                if [[ -z $2 || $2 = $SERVICE ]]; then
-                    processId=jps | grep $SERVICE | cut -f 1 --delimiter=" "
-                    if [[ -z $processId ]]; then
-                        echo -e "\e[32mINFO\e[39m: \e[1m$SERVICE\e[0m cids server process in $SERVICE_DIR/ gracefully stopped"
-                    else
-                        echo -e "\e[33mWARN\e[39m: forcibly stopping \e[1m$SERVICE\e[0m cids server process in $SERVICE_DIR/ (PID: $processId)"
-                        # run kill command 2 times 
-                        for k in 1 2
-                            do 
-                                kill -9  $processId
-                            done
-                    fi
-                fi
-            fi
-        done
-    ;;
-    
     start)
-        for SERVICE_DIR in *; do
-            if [[ -d $SERVICE_DIR && $SERVICE_DIR =~ $pattern ]]; then
-                SERVICE=$(cat ${CIDS_SERVER_DIR}/$SERVICE_DIR/cs_ctl.sh | grep SERVICE | cut -f 2 -d "=")
-                if [[ -z $2 || $2 = $SERVICE ]]; then
-                    echo -e "\e[32mINFO\e[39m: starting \e[1m$SERVICE\e[0m cids server process in $SERVICE_DIR/"
-                    $CIDS_SERVER_DIR/$SERVICE_DIR/cs_ctl.sh $1
-                    sleep 10
-                fi
-            fi
-        done
-
-    	ps -ef|grep 'D'${CIDS_ACCOUNT_EXTENSION}
+        SERVICE=$2        
+        start_server ${SERVICE}
     ;;
 	
+    stop)
+        SERVICE=$2
+        stop_server ${SERVICE}
+    ;;
+    
     restart)
-    	$0 stop $2
-	$0 start $2
+        SERVICE=$2
+        $0 stop  ${SERVICE}
+        $0 start ${SERVICE}
+    ;;
+
+    follow)
+        follow ${SERVICE}
+    ;;
+
+    log)
+        log ${SERVICE}
+    ;;
+
+    list_services)
+        list_services
     ;;
 
     *)
-	echo "Usage: $0 {start|stop|restart} {SERVICE (optional)}"
-	exit 1
+        echo "Usage: $0 {list_services|{start|stop|restart|log|follow} {SERVICE (optional)}}"
     ;;
 
 esac
--- copy/entrypoints/entrypoint_cids.sh
+++ copy/entrypoints/entrypoint_cids.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-export CIDS_STARTER_DIR=${CIDS_LIB_DIR}/starter${CIDS_ACCOUNT_EXTENSION}
-export START_OPTIONS=$*
+CIDS_CTL=${CIDS_DISTRIBUTION_DIR}/utils/cids_ctl.sh
 
-serversPath=${CIDS_DISTRIBUTION_DIR}/server
-cd $serversPath
-for serverDir in $(ls -1 . | grep -E "^[0-9]{3}_.*$" | sort); do
-  export SERVICE_DIR=$serversPath/$serverDir
-  export SERVICE=${serverDir:4}
-  export STARTER_JAR=${SERVICE}-starter.jar
-  export SLEEP_BEFORE_START=1
-
-  ENV_FILE=$serverDir/ENV
-  if [ -f ${ENV_FILE} ]; then
-    source ${ENV_FILE}
-  fi
-
-  sleep ${SLEEP_BEFORE_START}
-  ${CIDS_DISTRIBUTION_DIR}/utils/_cids_service_ctl.master.sh start
-  tail -f ${SERVICE_DIR}/$SERVICE.out &
+for SERVICE in $(${CIDS_CTL} list_services); do
+  ${CIDS_CTL} start ${SERVICE}
+  ${CIDS_CTL} follow ${SERVICE} &
 done
 
 echo -e "\n\e[32mhit [CTRL+C] to exit or run 'docker stop <container>'\e[39m:\n"
@@ -26,16 +12,6 @@ trap true SIGTERM
 tail -f /dev/null &
 wait $!
 
-for serverDir in $(ls -1 . | grep -E "^[0-9]{3}_.*$" | sort -r); do
-  export SERVICE_DIR=$serversPath/$serverDir
-  export SERVICE=${serverDir:4}
-  export SLEEP_BEFORE_STOP=1
-
-  ENV_FILE=$serverDir/ENV
-  if [ -f ${ENV_FILE} ]; then
-    source ${ENV_FILE}
-  fi
-
-  sleep ${SLEEP_BEFORE_STOP}
-  ${CIDS_DISTRIBUTION_DIR}/utils/_cids_service_ctl.master.sh stop
+for SERVICE in $(${CIDS_CTL} list_services); do
+  ${CIDS_CTL} stop ${SERVICE}
 done
