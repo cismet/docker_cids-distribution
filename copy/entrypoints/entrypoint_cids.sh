ba832d533ff4e4c6a5d9145620e0ff7827cf9881
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2023-05-02 09:04:23 +0200

    adding exec_before_start

--- copy/entrypoints/entrypoint_cids.sh
+++ copy/entrypoints/entrypoint_cids.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -f "${EXEC_BEFORE_START}" ]; then
+  echo "\e[32mINFO\e[39m: executing ${EXEC_BEFORE_START}"
+  ${EXEC_BEFORE_START} || exit 1;
+fi
+
 CIDS_CTL=${CIDS_DISTRIBUTION_DIR}/utils/cids_ctl.sh
 
 for SERVICE in $(${CIDS_CTL} list_services); do
@@ -8,7 +13,7 @@ for SERVICE in $(${CIDS_CTL} list_services); do
   ${CIDS_CTL} follow ${SERVICE} &
 done
 
-echo -e "\n\e[32mhit [CTRL+C] to exit or run 'docker stop <container>'\e[39m:\n"
+echo "\n\e[32mhit [CTRL+C] to exit or run 'docker stop <container>'\e[39m:\n"
 trap true SIGTERM
 tail -f /dev/null &
 wait $!
--- copy/entrypoints/entrypoint_jnlp.sh
+++ copy/entrypoints/entrypoint_jnlp.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!bin/bash
 
-echo -e "\e[32mINFO\e[39m: starting nginx"
+if [ -f "${EXEC_BEFORE_START}" ]; then
+  echo "\e[32mINFO\e[39m: executing ${EXEC_BEFORE_START}"
+  ${EXEC_BEFORE_START} || exit 1;
+fi
+
+echo "\e[32mINFO\e[39m: starting nginx"
 sed -i -- "s#__CIDS_DISTRIBUTION_DIR__#${CIDS_DISTRIBUTION_DIR:-/cidsDistribution}#g" /etc/nginx/sites-available/default
 
 nginx -g 'daemon off;'
