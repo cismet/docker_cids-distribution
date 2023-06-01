96c1a3c46d831e73d20d11beae1150f9b2aed788
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2023-06-01 10:21:52 +0200

    deployChanged on start

--- copy/entrypoints/entrypoint_jnlp.sh
+++ copy/entrypoints/entrypoint_jnlp.sh
@@ -5,6 +5,9 @@ if [ ! -z "${EXEC_BEFORE_START}" ]; then
   bash -c "${EXEC_BEFORE_START}" || exit 1;
 fi
 
+if [ ! -z "${CLIENT_RESOURCES_PLAIN}" ]; then
+  /cidsDistribution/utils/res_ctl.sh deployChanged ${CLIENT_RESOURCES_PLAIN}
+fi
 echo -e "\e[32mINFO\e[39m: starting nginx"
 sed -i -- "s#__CIDS_DISTRIBUTION_DIR__#${CIDS_DISTRIBUTION_DIR:-/cidsDistribution}#g" /etc/nginx/sites-available/default
 
